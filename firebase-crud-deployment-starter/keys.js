ccae929246442a4e5bd1614bbb72744f82cfd3ab
Abdulsalam Y Alahmri <salam9@outlook.com>
2019-04-24 12:14:48 +0300

    hosting

---
+++ firebase-crud-deployment-starter/keys.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBKsj94Nc3PF1ajkrclYEtbGAv9K9SHzbI",
+    authDomain: "korn-firebase.firebaseapp.com",
+    databaseURL: "https://korn-firebase.firebaseio.com",
+    projectId: "korn-firebase",
+    storageBucket: "korn-firebase.appspot.com",
+    messagingSenderId: "781545314867"
+  };
